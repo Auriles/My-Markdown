6c4113b81ada8328f9a6f98eafb4ebc51f92cbbc
Auriles El Haddad <auriles0404@gmail.com>
2017-04-14 15:01:02 +0200

    states et modification de states

--- markdown/src/index.js
+++ markdown/src/index.js
@@ -3,21 +3,40 @@ import { render } from 'react-dom';
 // CSS
 import './style/css/bootstrap.min.css';
 import './index.css';
+// JS
+import { sampleText } from './sampleText';
 
 class App extends React.Component {
+
+  state = {
+    text: sampleText
+  };
+
+  editText = (event) => {
+    const text = event.target.value;
+    this.setState({ text });
+  };
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6">
-            <textarea name="" id="" cols="30" rows="35" value="Tapez votre texte" className="form-control"></textarea>
+            <textarea
+              cols="30"
+              rows="35"
+              value={this.state.text}
+              className="form-control"
+              onChange={(e) => this.editText(e)}>
+
+            </textarea>
           </div>
           <div className="col-sm-6">
-            <h1>Résultats</h1>
+            {this.state.text}
           </div>
         </div>
       </div>
-      )
+    )
   }
 }
 
