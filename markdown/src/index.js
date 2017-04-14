4807f7ca09d737ae156976d2150924a541e58ed9
Auriles El Haddad <auriles0404@gmail.com>
2017-04-14 14:36:14 +0200

    mise en place zone de saisie

--- markdown/src/index.js
+++ markdown/src/index.js
@@ -1,2 +1,27 @@
-// Ici on code notre App
-alert("Coucou");
+import React from 'react';
+import { render } from 'react-dom';
+// CSS
+import './style/css/bootstrap.min.css';
+import './index.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea name="" id="" cols="30" rows="35" value="Tapez votre texte" className="form-control"></textarea>
+          </div>
+          <div className="col-sm-6">
+            <h1>Résultats</h1>
+          </div>
+        </div>
+      </div>
+      )
+  }
+}
+
+render(
+  <App />,
+  document.getElementById('root')
+  );
